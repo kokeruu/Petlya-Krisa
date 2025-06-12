5a7cb1edeef61f0eebf385cadb72880113f7ac78
kokeruu <katyagulyaeva04@gmail.com>
2025-06-12 17:46:30 +0300

    gu

--- test/Assets/Scripts/CardCollection.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CardCollection", menuName = "Card Game/Card Collection")]
-public class CardCollection : ScriptableObject
-{
-    public List<CardData> cards;
-}
